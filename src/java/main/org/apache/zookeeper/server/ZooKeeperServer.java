29816337daeb0602a904cadca758b4c40ca84d27
Panagiotis Papadopoulos <panpap@gmx.com>
2014-07-15 17:21:35 +0300

    Update ZooKeeperServer.java

--- src/java/main/org/apache/zookeeper/server/ZooKeeperServer.java
+++ src/java/main/org/apache/zookeeper/server/ZooKeeperServer.java
@@ -272,7 +272,7 @@ public class ZooKeeperServer implements SessionExpirer, ServerStats.Provider {
     {
     	String nodeName="/state";
     	//panpap: Gets all watchers either dataWatchers or childWatchers
-    	ArrayList<InetSocketAddress> dataWatchers=this.zkDb.dataTree.getWatchersAddress(); 
+    	HashSet<InetSocketAddress> dataWatchers=this.zkDb.dataTree.getWatchersAddress(); 
     	System.out.println("\n\n\n\n\n\n TTTTTTT "+clnWatchers.size()+" "+System.currentTimeMillis()+"\n\n\n\n\n");
 
     	if ((serverStats()!=null)&&(getServerCnxnFactory()!=null)&&(this.serverMap!=null))
